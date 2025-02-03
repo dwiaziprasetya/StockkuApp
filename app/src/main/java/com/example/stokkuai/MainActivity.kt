3f068631140a0a7d80f637a58dc6f98a473042a4
Dwi Azi Prasetya <dwiaziprasetya456@gmail.com>
2025-02-03 23:06:31 +0700

    Add HomeScreen.kt

--- app/src/main/java/com/example/stokkuai/MainActivity.kt
+++ app/src/main/java/com/example/stokkuai/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.example.stokkuai.ui.screen.home.HomeScreen
 import com.example.stokkuai.ui.theme.StokkuAITheme
 
 class MainActivity : ComponentActivity() {
@@ -12,6 +13,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             StokkuAITheme {
+                HomeScreen()
             }
         }
     }
---
+++ app/src/main/java/com/example/stokkuai/ui/screen/home/HomeScreen.kt
@@ -0,0 +1,30 @@
+package com.example.stokkuai.ui.screen.home
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.stokkuai.ui.theme.StokkuAITheme
+
+@Composable
+fun HomeScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ){
+        Text(
+            text = "Home Screen",
+            modifier = Modifier.align(Alignment.Center)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun HomeScreenPreview() {
+    StokkuAITheme {
+        HomeScreen()
+    }
+}
