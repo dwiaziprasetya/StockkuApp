80a838a3b88f818b9547deedca6fd542d2dbd1ea
Dwi Azi Prasetya <dwiaziprasetya456@gmail.com>
2025-02-03 22:37:24 +0700

    Add navigation compose dependency

--- app/src/main/java/com/example/stokkuai/RootNavigationGraph.kt
+++ app/src/main/java/com/example/stokkuai/RootNavigationGraph.kt
@@ -1,2 +1,12 @@
 package com.example.stokkuai
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+
+@Composable
+fun RootNavigationGraph(
+    navController: NavHostController,
+) {
+    
+}
