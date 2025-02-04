d486dd34b69678cda9b269424831b3c37f345244
Dwi Azi Prasetya <dwiaziprasetya456@gmail.com>
2025-02-05 01:12:32 +0700

    Add StockkuAIData.kt and ImageSliderItem.kt file

---
+++ app/src/main/java/com/example/stokkuai/data/StokkuAIData.kt
@@ -0,0 +1,4 @@
+package com.example.stokkuai.data
+
+object StokkuAIData {
+}
---
+++ app/src/main/java/com/example/stokkuai/data/model/ImageSliderItem.kt
@@ -0,0 +1,7 @@
+package com.example.stokkuai.data.model
+
+data class ImageSliderItem(
+    val imageResId: Int,
+    val title: String,
+    val description: String
+)
